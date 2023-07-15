3638584bee3a94befc8eb0483d6f66b8a126ab3e
Coder2195 <coder2195mail@gmail.com>
2023-07-15 17:51:36 -0400

    scale down a bit for font

--- app/page.tsx
+++ app/page.tsx
@@ -10,19 +10,22 @@ const HomePage: FC = () => {
   const { status } = useSession();
   return (
     <Layout>
-      <h1 className="vscale">
-        Inspiring BTHS youth to make change in a unjust society.
+      <h1 className="mb-6 underline vscale decoration-blue-700 decoration-[2vw] 2xl:decoration-[26px]">
+        BTHS Repair the World
       </h1>
+      <h3 className="vscale">
+        Inspiring BTHS youth to make change in a unjust society.
+      </h3>
       <div className="relative my-3 w-full h-[50vw] lg:h-[600px]">
         <Image src="/images/home-1.png" fill className="object-cover" alt="" />
       </div>
-      <h1 className="vscale">Mobilizing them for productivity.</h1>
+      <h3 className="vscale">Mobilizing them for productivity.</h3>
       <div className="relative my-3 w-full h-[50vw] lg:h-[600px]">
         <Image src="/images/home-2.jpg" fill className="object-cover" alt="" />
       </div>
-      <h1 className="vscale">
+      <h3 className="vscale">
         BTHS Repair the World will set the stage for change.
-      </h1>
+      </h3>
 
       {status === "unauthenticated" && (
         <Button
@@ -31,7 +34,7 @@ const HomePage: FC = () => {
           color="amber"
           className="mt-3"
         >
-          <h3 className="vscale">Join us now!</h3>
+          <h4 className="vscale">Join us now!</h4>
         </Button>
       )}
     </Layout>
