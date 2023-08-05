67ed8e9005ef599857704b8336080c40093f2dbc
Coder2195 <coder2195mail@gmail.com>
2023-08-05 15:04:47 -0400

    module fix

---
+++ src/app/events/page.tsx
@@ -0,0 +1,12 @@
+import Layout from "@/components/Layout";
+import { FC } from "react";
+
+const HomePage: FC = () => {
+  return (
+    <Layout>
+      <h1>Events</h1>
+    </Layout>
+  );
+};
+
+export default HomePage;
