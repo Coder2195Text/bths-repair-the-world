e838be0dc047721613f9b3c8dab6c093aa66ec3a
Coder2195 <coder2195mail@gmail.com>
2023-08-07 20:51:54 -0400

    new

--- src/app/api/events/route.ts
+++ src/app/api/events/route.ts
@@ -12,7 +12,7 @@ const schema = Joi.object({
   eventTime: Joi.date().iso().required(),
 });
 
-type EventWriteBody = Omit<
+type EventPOSTBody = Omit<
   Event,
   "id" | "createdAt" | "description" | "createdAt"
 > & {
@@ -20,29 +20,13 @@ type EventWriteBody = Omit<
   eventTime: string;
 };
 
-type EventPatchBody = Partial<EventWriteBody>;
+type EventPATCHBody = Partial<EventPOSTBody>;
 
 async function handler(method: "GET" | "POST", req: NextRequest) {
   const { searchParams } = new URL(req.nextUrl);
   const skip = Number(searchParams.get("page")) || 0;
   if (skip < 0)
     return NextResponse.json({ error: "Bad page" }, { status: 400 });
-
-  const allowed = await getServerSession({ ...AUTH_OPTIONS }).then(
-    async (s) => {
-      if (!s?.user.email) return false;
-      if (method === "GET") return true;
-      return await prisma.user
-        .findUnique({
-          where: { email: s.user.email },
-          select: { position: true },
-        })
-        .then((u) => ["admin", "exec"].includes(u?.position!));
-    }
-  );
-
-  if (!allowed)
-    return NextResponse.json({ error: "Not authenticated" }, { status: 401 });
 
   if (method === "GET") {
     const events = (
@@ -59,56 +43,69 @@ async function handler(method: "GET" | "POST", req: NextRequest) {
     return NextResponse.json(events, { status: 200 });
   }
 
-  if (method === "POST") {
-    if (!req.body)
-      return NextResponse.json({ error: "Missing body" }, { status: 400 });
+  const allowed = await getServerSession({ ...AUTH_OPTIONS }).then(
+    async (s) => {
+      if (!s?.user.email) return false;
+      return await prisma.user
+        .findUnique({
+          where: { email: s.user.email },
+          select: { position: true },
+        })
+        .then((u) => ["admin", "exec"].includes(u?.position!));
+    },
+  );
 
-    const result = req.body
-      .getReader()
-      .read()
-      .then((r) => r.value && new TextDecoder().decode(r.value))
-      .then(function(val): [boolean, any] {
-        let parsed;
-        if (!val) return [false, "Missing body"];
-        try {
-          parsed = JSON.parse(val);
-        } catch (e) {
-          return [false, "Bad JSON"];
-        }
-        let errors = schema.validate(parsed).error;
-        return errors ? [false, errors] : [true, parsed];
+  if (!allowed)
+    return NextResponse.json({ error: "Not authenticated" }, { status: 401 });
+
+  if (!req.body)
+    return NextResponse.json({ error: "Missing body" }, { status: 400 });
+
+  const result = req.body
+    .getReader()
+    .read()
+    .then((r) => r.value && new TextDecoder().decode(r.value))
+    .then(function(val): [boolean, any] {
+      let parsed;
+      if (!val) return [false, "Missing body"];
+      try {
+        parsed = JSON.parse(val);
+      } catch (e) {
+        return [false, "Bad JSON"];
+      }
+      let errors = schema.validate(parsed).error;
+      return errors ? [false, errors] : [true, parsed];
+    });
+
+  if ((await result)[0]) {
+    const rawData: EventPOSTBody | EventPATCHBody = (await result)[1];
+
+    const newData: Omit<Event, "id" | "createdAt"> = {
+      ...(rawData as EventPOSTBody),
+      description: Buffer.from(rawData.description!),
+      eventTime: new Date(rawData.eventTime!),
+    };
+
+    try {
+      const body = await prisma.event.create({
+        data: newData,
       });
 
-    if ((await result)[0]) {
-      const rawData: EventWriteBody = (await result)[1];
-
-      const newData: Omit<Event, "id" | "createdAt"> = {
-        ...rawData,
-        description: Buffer.from(rawData.description),
-        eventTime: new Date(rawData.eventTime),
-      };
-
-      try {
-        const body = await prisma.event.create({
-          data: newData,
-        });
-
-        return NextResponse.json(
-          {
-            ...body,
-            description: body.description.toString(),
-          },
-          {
-            status: 200,
-          }
-        );
-      } catch (e) {
-        console.log(e);
-        return NextResponse.json({ error: e }, { status: 500 });
-      }
+      return NextResponse.json(
+        {
+          ...body,
+          description: body.description.toString(),
+        },
+        {
+          status: 200,
+        },
+      );
+    } catch (e) {
+      console.log(e);
+      return NextResponse.json({ error: e }, { status: 500 });
     }
-    return NextResponse.json({ error: (await result)[1] }, { status: 400 });
   }
+  return NextResponse.json({ error: (await result)[1] }, { status: 400 });
 }
 
 export const GET = handler.bind(null, "GET");
--- src/app/api/users/[email]/route.ts
+++ src/app/api/users/[email]/route.ts
@@ -16,7 +16,7 @@ const POSTSchema = Joi.object({
     .required()
     .regex(/^[A-Za-z]\d[A-Za-z]$/),
   birthday: Joi.string().regex(
-    /\b\d{4}-(?:1[0-2]|0?[1-9])-(?:3[0-1]|[12][0-9]|0?[1-9])\b/
+    /\b\d{4}-(?:1[0-2]|0?[1-9])-(?:3[0-1]|[12][0-9]|0?[1-9])\b/,
   ),
   referredBy: Joi.string()
     .optional()
@@ -33,7 +33,7 @@ const PATCHSchema = Joi.object({
   preferredName: Joi.string().max(190),
   prefect: Joi.string().regex(/^[A-Za-z]\d[A-Za-z]$/),
   birthday: Joi.string().regex(
-    /\b\d{4}-(?:1[0-2]|0?[1-9])-(?:3[0-1]|[12][0-9]|0?[1-9])\b/
+    /\b\d{4}-(?:1[0-2]|0?[1-9])-(?:3[0-1]|[12][0-9]|0?[1-9])\b/,
   ),
   referredBy: Joi.string()
     .email()
@@ -45,7 +45,7 @@ const PATCHSchema = Joi.object({
 async function handler(
   method: "GET" | "PATCH" | "POST",
   req: NextRequest,
-  { params: { email } }: Params
+  { params: { email } }: Params,
 ) {
   if (!email) {
     return NextResponse.json({ error: "Email is required" }, { status: 400 });
@@ -69,7 +69,7 @@ async function handler(
           select: { position: true },
         })
         .then((u) => ["admin", "exec"].includes(u?.position!));
-    }
+    },
   );
 
   if (!allowed) {
@@ -94,75 +94,73 @@ async function handler(
     return NextResponse.json(user && { ...user, referrals }, { status: 200 });
   }
 
-  if (method === "PATCH" || method === "POST") {
-    if (!req.body)
-      return NextResponse.json({ error: "Missing body" }, { status: 400 });
+  if (!req.body)
+    return NextResponse.json({ error: "Missing body" }, { status: 400 });
 
-    const result: Promise<[false, string] | [true, UserWriteBody]> = req.body
-      .getReader()
-      .read()
-      .then((r) => r.value && new TextDecoder().decode(r.value))
-      .then(function(val): [boolean, any] {
-        let parsed;
-        if (!val) return [false, "Missing body"];
-        try {
-          parsed = JSON.parse(val);
-        } catch (e) {
-          return [false, "Bad JSON"];
-        }
-        let errors = (method === "POST" ? POSTSchema : PATCHSchema).validate(
-          parsed
-        ).error;
-        return errors ? [false, errors] : [true, parsed];
-      });
-
-    if ((await result)[0]) {
-      const data = (await result)[1] as UserWriteBody;
-
+  const result: Promise<[false, string] | [true, UserWriteBody]> = req.body
+    .getReader()
+    .read()
+    .then((r) => r.value && new TextDecoder().decode(r.value))
+    .then(function(val): [boolean, any] {
+      let parsed;
+      if (!val) return [false, "Missing body"];
       try {
-        const [body, referrals] = await Promise.all([
-          method === "POST"
-            ? prisma.user.create({
-              data: {
-                ...data,
-                email,
-                preferredName: data.preferredName || data.name,
-              },
-            })
-            : prisma.user.update({
-              where: { email },
-              data: {
-                ...data,
-                preferredName: data.preferredName || data.name,
-                lastUpdated: new Date().toISOString(),
-              },
-            }),
-          prisma.user
-            .findMany({
-              where: { referredBy: email },
-              select: {
-                email: true,
-              },
-            })
-            .then((u) => u.map((u) => u.email)),
-        ]);
-
-        return NextResponse.json(
-          {
-            ...body,
-            referrals,
-          },
-          {
-            status: 200,
-          }
-        );
+        parsed = JSON.parse(val);
       } catch (e) {
-        console.log(e);
-        return NextResponse.json({ error: e }, { status: 500 });
+        return [false, "Bad JSON"];
       }
+      let errors = (method === "POST" ? POSTSchema : PATCHSchema).validate(
+        parsed,
+      ).error;
+      return errors ? [false, errors] : [true, parsed];
+    });
+
+  if ((await result)[0]) {
+    const data = (await result)[1] as UserWriteBody;
+
+    try {
+      const [body, referrals] = await Promise.all([
+        method === "POST"
+          ? prisma.user.create({
+            data: {
+              ...data,
+              email,
+              preferredName: data.preferredName || data.name,
+            },
+          })
+          : prisma.user.update({
+            where: { email },
+            data: {
+              ...data,
+              preferredName: data.preferredName || data.name,
+              lastUpdated: new Date().toISOString(),
+            },
+          }),
+        prisma.user
+          .findMany({
+            where: { referredBy: email },
+            select: {
+              email: true,
+            },
+          })
+          .then((u) => u.map((u) => u.email)),
+      ]);
+
+      return NextResponse.json(
+        {
+          ...body,
+          referrals,
+        },
+        {
+          status: 200,
+        },
+      );
+    } catch (e) {
+      console.log(e);
+      return NextResponse.json({ error: e }, { status: 500 });
     }
-    return NextResponse.json({ error: (await result)[1] }, { status: 400 });
   }
+  return NextResponse.json({ error: (await result)[1] }, { status: 400 });
 }
 
 export const GET = handler.bind(null, "GET");
