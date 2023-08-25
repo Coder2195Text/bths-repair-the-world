05d5815edaceeedfa3311a8a3d27d455b5b55139
Coder2195 <coder2195mail@gmail.com>
2023-08-24 21:35:51 -0400

    padding and stuff

--- src/app/HomePage.tsx
+++ src/app/HomePage.tsx
@@ -137,7 +137,7 @@ const HomePage: FC = () => {
 
   return (
     <>
-      <div className="relative w-full h-[75vw] lg:h-[720px] rounded-xl mb-3 mt-68">
+      <div className="relative w-full h-[40vw] rounded-xl mb-3 mt-[68px]">
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full text-center ">
           <h1 className="title">BTHS Repair the World</h1>
           <h4>
