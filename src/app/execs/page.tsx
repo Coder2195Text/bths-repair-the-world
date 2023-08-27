a84dfb3a2423a7d514e513a242e39d79edd9721a
Coder2195 <coder2195mail@gmail.com>
2023-08-27 16:10:33 -0400

    metadata forgot lol

--- src/app/execs/page.tsx
+++ src/app/execs/page.tsx
@@ -3,13 +3,16 @@ import Layout from "@/components/Layout";
 import { ExecList } from "./components";
 import { ExecDetails, User } from "@prisma/client";
 
+export const metadata = {
+  title: "Events - BTHS Repair the World",
+  description: "Check out some of our events!",
+};
+
 const ExecsPage: FC = () => {
   return (
     <Layout>
-      <div>
-        <h1>Our Exec Team</h1>
-        <ExecList />
-      </div>
+      <h1>Our Exec Team</h1>
+      <ExecList />
     </Layout>
   );
 };
