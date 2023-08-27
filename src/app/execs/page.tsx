7445880579d172b1f45fd9f8cccd6305d92aeca0
Coder2195 <coder2195mail@gmail.com>
2023-08-27 14:47:23 -0400

    try revalidate + dynamic

--- src/app/execs/page.tsx
+++ src/app/execs/page.tsx
@@ -3,6 +3,7 @@ import Layout from "@/components/Layout";
 import { ExecList } from "./server-components";
 
 export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 export const metadata = {
   title: "Executives - BTHS Repair the World",
