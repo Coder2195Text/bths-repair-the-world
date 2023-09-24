062e198606f954a98a8a4c2a1bb87dbe392d3a00
Coder2195 <coder2195mail@gmail.com>
2023-09-23 21:25:57 -0400

    try nother

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -4,7 +4,6 @@ import { JWT } from "next-auth/jwt";
 declare module "next-auth" {
   interface Session {
     user: {
-      /** The user's postal address. */
       id: string;
     } & DefaultSession["user"];
   }
